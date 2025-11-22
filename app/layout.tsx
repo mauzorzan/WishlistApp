16eb9192a25a5a5d30505b62275e8522b3f0c5cf
mauriciozorzan <dr.maukz@gmail.com>
2025-11-21 18:28:46 -0800

    Snebby's Wishlist App

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "My Wishlist App",
+  title: "Snebby's Wishlist App",
   description: "A beautiful wishlist app to save all the things you want",
 };
 
